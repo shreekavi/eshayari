96c4b22f1aa031a4b97381e45520ee89b85103d2
Shree Kavi <shreekavi@gmail.com>
2012-03-01 13:26:29 +0530

    Trying gObject Mainloop

--- eshayari.py
+++ eshayari.py
@@ -2,5 +2,12 @@
 
 import os
 import sys
+import gobject
+gobject.threads_init()
+
 import eshayari
 eshayari.main.main(sys.argv[1:])
+
+g_loop = threading.Thread(target=gobject.MainLoop().run)
+g_loop.daemon = True
+g_loop.start()
